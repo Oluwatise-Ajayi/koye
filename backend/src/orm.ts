730003db9612bfe9b9d55cf667438a857d40897d
Fakorode Odunayo Henry <156727288+HenryTech12@users.noreply.github.com>
2025-09-19 03:09:27 +0100

    Format DataSource initialization for readability

--- backend/src/orm.ts
+++ backend/src/orm.ts
@@ -2,4 +2,11 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { Art } from './entities/Art';
 import { User } from './entities/User';
-export const AppDataSource = new DataSource({ type: 'postgres'}, url: process.env.DATABASE_URL, synchronize: true, entities: [Art, User], logging: true });
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  synchronize: true,
+  entities: [Art, User],
+  logging: true
+});
