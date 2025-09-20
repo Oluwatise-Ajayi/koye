28c1090f94f4463c3c358be8b72f481d7619ac08
Oluwatise Ajayi <dr393462@gmail.com>
2025-09-20 18:00:00 +0100

    commiting on the old version

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,8 +1,8 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn('uuid') id: string;
-  @Column({unique:true, nullable:true}) username: string;
-  @Column({nullable:true}) walletAddress: string;
-  @CreateDateColumn() createdAt: Date;
+  @PrimaryGeneratedColumn('uuid') id!: string;
+  @Column({unique:true, nullable:true}) username?: string;
+  @Column({nullable:true}) walletAddress?: string;
+  @CreateDateColumn() createdAt!: Date;
 }
